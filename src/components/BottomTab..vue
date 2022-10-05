961e2370dc9283fee73d8fbf6420410a338580e1
Meruem117 <1572677319@qq.com>
2022-10-05 23:25:46 +0800

    add component

---
+++ src/components/BottomTab..vue
@@ -0,0 +1,16 @@
+<template>
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+import type { activityItem } from '@/models'
+
+const router = useRouter()
+const props = defineProps<{
+  data: activityItem[]
+}>()
+</script>
+
+<style scoped lang="less">
+
+</style>
