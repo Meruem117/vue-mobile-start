7d76b10115ad81ed1000b9cee950ab1d0a43c7c6
Meruem117 <1572677319@qq.com>
2022-10-08 21:46:07 +0800

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,26 +6,15 @@
 import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import BottomTab from '@/components/BottomTab..vue'
-import type { reviewItem } from '@/models'
-import { getReviewList } from '@/services'
 
 type stateType = {
-  location: string,
-  notice: string,
-  data: reviewItem[]
 }
 
 const router = useRouter()
 const state: stateType = reactive({
-  location: '',
-  notice: '',
-  data: []
 })
 
 async function init() {
-  state.location = 'XXXXXXXX'
-  state.notice = 'xxxxxxxxxxxxxxxx'
-  state.data = await getReviewList()
 }
 
 init()
